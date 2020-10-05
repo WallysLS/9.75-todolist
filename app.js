829276712bacdd9663615644401080b6d97b7779
Wallys L Souza <wallys31.souza@gmail.com>
2020-10-05 18:57:27 -0300

    Created the first event listener

---
+++ app.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const input = document.querySelector(".input");
+const add_button = document.querySelector(".add-button");
+
+add_button.addEventListener("click", function () {
+  alert("Hi");
+});
