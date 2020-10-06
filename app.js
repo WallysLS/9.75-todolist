49bea09ffd69c0e908d853d1fa6a95aaf4b96099
Wallys L Souza <wallys31.souza@gmail.com>
2020-10-06 08:40:49 -0300

    Created the function responsable for adding new todos

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const container = document.querySelector(".container");
 const input = document.querySelector(".input");
 const add_button = document.querySelector(".add-button");
+const listOftodos = document.querySelector(".listOfTodos");
 
-add_button.addEventListener("click", function () {
-  alert("Hi");
-});
+
+add_button.addEventListener("click", addNewTodo);
+
+function addNewTodo() {
+  var todoList = document.createElement("ul")
+  var todo = document.createElement("li")
+  todoList.appendChild(todo)
+
+  if (input.value === "") {
+    alert("I'm sorry, your todo can't be empty")
+  } else {
+    todo.innerHTML = input.value
+    document.body.insertBefore(todoList, listOftodos);
+
+    input.value = ""
+  }
+
+
+
+}
