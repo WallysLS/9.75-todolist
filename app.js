f5924f9abecebeb31a441eed66e1185b2a71f7b0
Wallys L Souza <wallys31.souza@gmail.com>
2020-10-07 11:40:20 -0300

    Created the functionality of checking and deleting the todos

--- app.js
+++ app.js
@@ -8,29 +8,44 @@ add_button.addEventListener("click", addNewTodo);
 listOfTodos.addEventListener("click", checkAndDeleteTodo);
 
 function addNewTodo() {
-  const todoList = document.createElement("div")
+  const todoDiv = document.createElement("div")
+  todoDiv.classList.add("todoDiv")
   const todo = document.createElement("li")
+  todo.classList.add("todo-item")
 
   if (input.value.trim() === "") {
     alert("I'm sorry, your todo can't be empty")
   } else {
     todo.innerHTML = input.value
 
-    todoList.appendChild(todo)
+    todoDiv.appendChild(todo)
 
     input.value = ""
 
     const checkButton = document.createElement("button")
     checkButton.innerHTML = `<i class="fas fa-check"></i>`
     checkButton.classList.add("complete-btn")
-    todoList.appendChild(checkButton)
+    todoDiv.appendChild(checkButton)
 
 
     const deleteButton = document.createElement("button")
     deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
     deleteButton.classList.add("trash-btn")
-    todoList.appendChild(deleteButton)
+    todoDiv.appendChild(deleteButton)
 
-    listOfTodos.appendChild(todoList)
+    listOfTodos.appendChild(todoDiv)
+  }
+}
+
+
+function checkAndDeleteTodo(event) {
+  const item = event.target;
+  if (item.classList == "complete-btn") {
+    item.parentElement.classList.toggle("completed")
+  }
+
+  if (item.classList == "trash-btn") {
+    item.parentElement.remove()
+
   }
 }
