faf12501901006bea9657c8494dae26c2865a889
Wallys L Souza <wallys31.souza@gmail.com>
2020-10-07 11:04:33 -0300

    Fixed the addNewTodo function

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
 const container = document.querySelector(".container");
 const input = document.querySelector(".input");
 const add_button = document.querySelector(".add-button");
-const listOftodos = document.querySelector(".todo-list");
+const listOfTodos = document.querySelector(".todo-list");
 
 
 add_button.addEventListener("click", addNewTodo);
-listOftodos = document.addEventListener("click", deleteTodo)
+listOfTodos.addEventListener("click", checkAndDeleteTodo);
 
 function addNewTodo() {
   const todoList = document.createElement("div")
   const todo = document.createElement("li")
-  todoList.appendChild(todo)
 
   if (input.value.trim() === "") {
     alert("I'm sorry, your todo can't be empty")
   } else {
     todo.innerHTML = input.value
-    document.body.insertBefore(todoList, listOftodos);
+
+    todoList.appendChild(todo)
 
     input.value = ""
 
+    const checkButton = document.createElement("button")
+    checkButton.innerHTML = `<i class="fas fa-check"></i>`
+    checkButton.classList.add("complete-btn")
+    todoList.appendChild(checkButton)
+
+
+    const deleteButton = document.createElement("button")
+    deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    deleteButton.classList.add("trash-btn")
+    todoList.appendChild(deleteButton)
+
+    listOfTodos.appendChild(todoList)
   }
-
-  const checkButton = document.createElement("button")
-  checkButton.innerHTML = `<i class="fas fa-check"></i>`
-  checkButton.classList.add("complete-btn")
-  todoList.appendChild(checkButton)
-
-
-  const deleteButton = document.createElement("button")
-  deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
-  deleteButton.classList.add("trash-btn")
-  todoList.appendChild(deleteButton)
-}
-
-function deleteTodo(event) {
-  const item = event.target
-  console.log(item)
-
 }
